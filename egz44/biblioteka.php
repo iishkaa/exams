571364844b773bd0cb04fe16d84266abeb0c51ed
iishkaa <iishkaa222@gmail.com>
2026-01-16 17:40:33 +0100

    egz44 - html/ css

---
+++ egz44/biblioteka.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Biblioteka miejska</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+<nav></nav>
+<section>
+    <h2>Liryka</h2>
+    <form action="" method="post">
+        <select name="lista" id="lista">
+            <!--skrypt2-->
+        </select>
+        <button>Rezerwuj</button> <!--wysyla do skrypt3-->
+        <!--skrypt3-->
+    </form>
+</section>
+<section>
+    <h2>Epika</h2>
+    <form action="" method="post">
+        <select name="lista2" id="lista2">
+            <!--skrypt2-->
+        </select>
+        <button>Rezerwuj</button><!--wysyla do skrypt3-->
+        <!--skrypt3-->
+    </form>
+</section>
+<section>
+    <h2>Dramat</h2>
+    <form action="" method="post">
+        <select name="lista3" id="lista3">
+            <!--skrypt2-->
+        </select>
+        <button>Rezerwuj</button><!--wysyla do skrypt3-->
+        <!--skrypt3-->
+    </form>
+</section>
+<section>
+    <h2>Zaległe książki</h2>
+    <ul>
+        <!--skrypt4-->
+    </ul>
+</section>
+<footer><strong>Autor: Michalina Wolna 3p_2</strong></footer>
+</body>
+</html>
